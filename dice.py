15805b5a71c3de414688e21da66f8fb52e51874b
Andrey G <andrey.grunyov@gmail.com>
2020-12-15 10:54:28 +0200

    Update dice.py

--- dice.py
+++ dice.py
@@ -1,5 +1,5 @@
-import RPi.GPIO as IO            # calling for header file which helps us use GPIO’s of PI
-import time                              # calling for time to provide delays in program
+import RPi.GPIO as IO        
+import time       
 from random import seed
 from random import randint
 
@@ -7,10 +7,10 @@ DISPLAY = [0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x67]
 
 def SETUP():
     seed(1)
-    IO.setwarnings(False)            # do not show any warnings
-    IO.setmode (IO.BCM)           # programming the GPIO by BCM pin numbers. (like PIN29 as‘GPIO5’)
+    IO.setwarnings(False)           
+    IO.setmode (IO.BCM)           
     IO.setup(15,IO.IN, pull_up_down=IO.PUD_DOWN)
-    IO.setup(13,IO.OUT)             # initialize GPIO Pins as outputs
+    IO.setup(13,IO.OUT)           
     IO.setup(6,IO.OUT)
     IO.setup(16,IO.OUT)
     IO.setup(20,IO.OUT)
@@ -18,6 +18,7 @@ def SETUP():
     IO.setup(19,IO.OUT)
     IO.setup(26,IO.OUT)
     IO.setup(12,IO.OUT)
+    SNAKE(0)
 
 
 def SNAKE(qty):
@@ -36,23 +37,19 @@ def SNAKE(qty):
     elif qty == 6:
         IO.output(13,0); IO.output(6,0); IO.output(16,0); IO.output(20,0); IO.output(21,0); IO.output(19,1); IO.output(26,0)
 
-
-
-
-
-def DICE():
+def randomDICE():
     value=randint(1,6)
     return value
 
-def PORT(pin):                    # assigning GPIO logic by taking 'pin' value
+def PORT(pin):                   
     if(pin&0x01 == 0x01):
-        IO.output(13,1)            # if  bit0 of 8bit 'pin' is true, pull PIN13 high
+        IO.output(13,1)          
     else:
-        IO.output(13,0)            # if  bit0 of 8bit 'pin' is false, pull PIN13 low
+        IO.output(13,0)          
     if(pin&0x02 == 0x02):
-        IO.output(6,1)             # if  bit1 of 8bit 'pin' is true, pull PIN6 high
+        IO.output(6,1)           
     else:
-        IO.output(6,0)            #if  bit1 of 8bit 'pin' is false, pull PIN6 low
+        IO.output(6,0)           
     if(pin&0x04 == 0x04):
         IO.output(16,1)
     else:
@@ -74,9 +71,9 @@ def PORT(pin):                    # assigning GPIO logic by taking 'pin' value
     else:
         IO.output(26,0)
     if(pin&0x80 == 0x80):
-        IO.output(12,1)            # if  bit7 of 8bit 'pin' is true, pull PIN12 high
+        IO.output(12,1)          
     else:
-        IO.output(12,0)            # if  bit7 of 8bit 'pin' is false, pull PIN12 low
+        IO.output(12,0)          
 
 def display_result(value):
     pin=DISPLAY[value]
@@ -89,35 +86,3 @@ def START():
         return True
     else:
         return False
-
-
-#while 1:
-#    if IO.input(10) == IO.HIGH :
-#        break
-
-SNAKE(0)
-
-while True:
-    if START():
-        break
-
-#start request
-
-
-#diceValue=DICE()
-#pin=DISPLAY[diceValue]
-#PORT(pin)
-
-#qty=1
-while 1:
-     SNAKE(qty)
-     qty=qty+1
-     time.sleep(0.1)
-     if qty == 7: qty = 1
-     
-
-#    for x in range(10):
-#        pin=DISPLAY[x]
-#        PORT(pin);
-        #time.sleep(0.3)
-
